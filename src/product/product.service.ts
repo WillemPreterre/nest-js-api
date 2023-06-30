4a4346b771430d95b228cd4a2c67523d727ad46c
WillemPreterre <wpreterre.lamanu@gmail.com>
2023-06-30 15:46:47 +0200

    optimisation + commentaire + update

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -10,57 +10,57 @@ export class ProductService {
 
   constructor() { }
 
-  // récupération du chemin vers le Json
+  // Récupération du chemin vers le fichier json
   private filePath = path.join(__dirname, '../../src/database/product.json');
   // Récupération des données du json
   private json = fs.readFileSync(this.filePath, 'utf8');
   // Création du json avec l'interface
   private jsonData: ProductInterface[] = JSON.parse(this.json);
 
-    // Function pour les messages d'erreurs
-    errorReport(error) {
-      throw new UnprocessableEntityException(error);
-    }
-    // Converti le js en json
-    stringifyJSON(data) {
-       return JSON.stringify(data)
-    }
+  // Function pour les messages d'erreurs
+  errorReport(error) {
+    throw new UnprocessableEntityException(error);
+  }
+  // Converti le js en json
+  stringifyJSON(data) {
+    return JSON.stringify(data)
+  }
+  // Permet d'ajouter un élément dans le fichier json
+  addToJson(objectToJson) {
+    fs.writeFileSync(this.filePath, objectToJson)
 
-    addToJson(objectToJson) {
-      fs.writeFileSync(this.filePath, objectToJson)
-
-    }
+  }
 
 
   create(productInterface: ProductInterface) {
 
-    // permet d'ajouter automatiquement un id en fonction du dernière élément créée
-    const maxId: number = Math.max(...this.jsonData.map((product) => product.id), 0);
+    // Permet d'ajouter automatiquement un id en fonction du dernier élément créée
+    const maxId: number = Math.max(...this.jsonData.map((product) => product.id));
     const id: number = maxId + 1;
     // Permet d'ajouter l'id et les informations de mon produit 
     const createProduct: ProductInterface = {
-      ...productInterface,
       id,
+      ...productInterface,
     }
     // Ajoute la nouvelle donnée dans l'array
     this.jsonData.push(createProduct)
-    //  conversion de ma donnée en json
+    //  Conversion de ma donnée en json
     const objectToJson = this.stringifyJSON(this.jsonData)
     // Ajoute la donnée dans le json
     this.addToJson(objectToJson)
     return objectToJson;
   }
 
-  // récupération de tous les produits
+  // Récupération de tous les produits
   findAll(): Array<ProductInterface> {
-    // récupérer seulement les articles disponible
+    // Récupérer seulement les articles disponible
     const filterProduct = this.jsonData.filter(produit => produit.quantity > 0)
     return filterProduct
   }
 
   // Récupération d'un produit
   findOne(id: number) {
-    // récupération du produit souhaité grâce à l'id
+    // Récupération du produit souhaité grâce à l'id
     const productId: ProductInterface = this.jsonData.find(product => product.id === id)
     // Erreur si l'id n'existe pas
     if (productId === undefined) {
@@ -70,16 +70,34 @@ export class ProductService {
   }
 
   // Modification produit
-  update(id: number, updateProductDto: UpdateProductDto) {
-    const productId = this.jsonData.findIndex(product => product.id === id)
-    if (productId === undefined) {
-      this.errorReport("ce produit n'existe pas")
+  update(id: number, productInterface: ProductInterface) {
+    // Je récupère l'index de l'élément avec l'ID spécifié
+    const index = this.jsonData.findIndex(product => product.id === id);
+
+    if (id === -1) {
+      // L'élément n'existe pas, générer une erreur
+      this.errorReport("Ce produit n'existe pas");
     }
+
+    // Je récupère les infos de mon produit
+    const updatedProduct = {
+      id,
+      ...productInterface,
+    };
+
+    // Je remplace l'élément existant par le nouveau produit mis à jour
+    this.jsonData[index] = updatedProduct;
+
+    const objectToJson = this.stringifyJSON(this.jsonData);
+
+    this.addToJson(objectToJson);
+
+    return updatedProduct;
   }
 
   // Suppression produit
   remove(id: number) {
-    // Récupération du produit que l'on veut supprimer
+    // Récupération de l'id du produit que l'on veut supprimer
     const productId = this.jsonData.findIndex(product => product.id === id)
 
     // Erreur si le produit n'existe pas
@@ -88,7 +106,7 @@ export class ProductService {
     }
     // Suppression du produit avec l'id
     this.jsonData.splice(productId, 1)
-    // on update le json
+    // On update le json
     const objectToJson = this.stringifyJSON(this.jsonData)
     // Ajout du nouveau tableau
     this.addToJson(objectToJson)
